802352718f6baf1189391a42bc7901dbe31553fc
tmttan03 <tmttan03@gmail.com>
2019-02-13 18:10:37 +0800

    Allow superusers to create postings

--- home/models.py
+++ home/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from posts.models import PostPage
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = PostPage.objects.filter()
+        context['blogpages'] = blogpages
+        return context
