366edad5aeb01718e89985110a36134239dbbb0f
tmttan03 <tmttan03@gmail.com>
2019-02-14 16:19:45 +0800

    Created models for categories, products or postings, album, and tags

--- marketplace/settings/base.py
+++ marketplace/settings/base.py
@@ -162,3 +162,6 @@ WAGTAIL_SITE_NAME = "marketplace"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+
+WAGTAILIMAGES_MAX_UPLOAD_SIZE = 15 * 1024 * 1024 
--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.db import models
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
 # New imports added for ClusterTaggableManager, TaggedItemBase, MultiFieldPanel
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
-from taggit.models import TaggedItemBase
+from taggit.models import TaggedItemBase, Tag
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
@@ -13,7 +15,7 @@ from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
-
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 
 @register_snippet
 class PostCategory(models.Model):
@@ -35,31 +37,104 @@ class PostCategory(models.Model):
         verbose_name_plural = 'product categories'
 
 
-class PostIndexPage(Page):
-    intro = RichTextField(blank=True)
+class PostIndexPage(RoutablePageMixin, Page):
+    """
+    Index page for posts.
+    We need to alter the page model's context to return the child page objects,
+    the BlogPage objects, so that it works as an index page
 
+    RoutablePageMixin is used to allow for a custom sub-URL for the tag views
+    defined above.
+    """
+    introduction = models.TextField(
+        help_text='Text to describe the page',
+        blank=True)
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction', classname="full"),
+    ]
+
+    # Speficies that only BlogPage objects can live under this index page
+    subpage_types = ['PostPage']
+
+    # Defines a method to access the children of the page (e.g. BlogPage
+    # objects). On the demo site we use this on the HomePage
+    def children(self):
+        return self.get_children().specific().live()
+
+    # Overrides the context to list all child items, that are live, by the
+    # date that they were published
+    # http://docs.wagtail.io/en/latest/getting_started/tutorial.html#overriding-context
     def get_context(self, request):
-        # Update context to include only published posts, ordered by reverse-chron
-        context = super().get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
-        context['blogpages'] = blogpages
+        context = super(PostIndexPage, self).get_context(request)
+        context['posts'] = PostPage.objects.descendant_of(
+            self).live().order_by(
+            '-date')
         return context
+
+    # This defines a Custom view that utilizes Tags. This view will return all
+    # related BlogPages for a given Tag or redirect back to the BlogIndexPage.
+    # More information on RoutablePages is at
+    # http://docs.wagtail.io/en/latest/reference/contrib/routablepage.html
+    
+    @route('^tags/$', name='tag_archive')
+    @route('^tags/([\w-]+)/$', name='tag_archive')
+    def tag_archive(self, request, tag=None):
+
+        try:
+            tag = Tag.objects.get(slug=tag)
+        except Tag.DoesNotExist:
+            if tag:
+                msg = 'There are no posts tagged with "{}"'.format(tag)
+                messages.add_message(request, messages.INFO, msg)
+            return redirect(self.url)
+
+        posts = self.get_posts(tag=tag)
+        context = {
+            'tag': tag,
+            'posts': posts
+        }
+        return render(request, 'posts/post_index_page.html', context)
+
+    def serve_preview(self, request, mode_name):
+        # Needed for previews to work
+        return self.serve(request)
+
+    # Returns the child BlogPage objects for this BlogPageIndex.
+    # If a tag is used then it will filter the posts by tag.
+    def get_posts(self, tag=None):
+        posts = PostPage.objects.live().descendant_of(self)
+        if tag:
+            posts = posts.filter(tags=tag)
+        return posts
+
+    # Returns the list of Tags for all child posts of this BlogPage.
+    def get_child_tags(self):
+        tags = []
+        for post in self.get_posts():
+            # Not tags.append() because we don't want a list of lists
+            tags += post.get_tags
+        tags = sorted(set(tags))
+        return tags
 
 
 class PostPageTag(TaggedItemBase):
-    content_object = ParentalKey(
-        'PostPage',
-        related_name='tagged_items',
-        on_delete=models.CASCADE
-    )
+    """
+    This model allows us to create a many-to-many relationship between
+    the PostPage object and tags. There's a longer guide on using it at
+    http://docs.wagtail.io/en/latest/reference/pages/model_recipes.html#tagging
+    """
+    content_object = ParentalKey('PostPage', related_name='tagged_items', on_delete=models.CASCADE)
 
 
 class PostPage(Page):
     date = models.DateField("Post date")
-    intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
     tags = ClusterTaggableManager(through=PostPageTag, blank=True)
-    categories = ParentalManyToManyField('posts.PostCategory', blank=True)
+    categories = models.ForeignKey('posts.PostCategory', blank=True, null=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=250, default="Davao")
+    seller = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
 
     def main_image(self):
         gallery_item = self.gallery_images.first()
@@ -69,20 +144,45 @@ class PostPage(Page):
             return None
 
     search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
+        index.SearchField('body')
     ]
 
     content_panels = Page.content_panels + [
-        MultiFieldPanel([
-            FieldPanel('date'),
-            FieldPanel('tags'),
-            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
-        ], heading="Blog information"),
-        FieldPanel('intro'),
+        FieldPanel('date'),
         FieldPanel('body'),
+        MultiFieldPanel([
+            FieldPanel('price'),
+            FieldPanel('location'),
+            FieldPanel('seller'),
+            FieldPanel('categories'),
+            FieldPanel('tags'),
+        ], heading="Post information"),
         InlinePanel('gallery_images', label="Gallery images"),
+        
     ]
+
+    @property
+    def get_tags(self):
+        """
+        Similar to the authors function above we're returning all the tags that
+        are related to the blog post into a list we can access on the template.
+        We're additionally adding a URL to access BlogPage objects with that tag
+        """
+        tags = self.tags.all()
+        for tag in tags:
+            tag.url = '/'+'/'.join(s.strip('/') for s in [
+                self.get_parent().url,
+                'tags',
+                tag.slug
+            ])
+        return tags
+
+    # Specifies parent to BlogPage as being BlogIndexPages
+    parent_page_types = ['PostIndexPage']
+
+    # Specifies what content types can exist as children of BlogPage.
+    # Empty list means that no child content types are allowed.
+    subpage_types = []
 
     
 class PostPageGalleryImage(Orderable):
@@ -98,7 +198,7 @@ class PostPageGalleryImage(Orderable):
     ]
 
 
-class PostTagIndexPage(Page):
+class TagsIndexPage(Page):
 
     def get_context(self, request):
 
@@ -107,6 +207,6 @@ class PostTagIndexPage(Page):
         blogpages = PostPage.objects.filter(tags__name=tag)
 
         # Update template context
-        context = super().get_context(request)
-        context['blogpages'] = blogpages
+        #context = super().get_context(request)
+        #context['blogpages'] = blogpages
         return context
